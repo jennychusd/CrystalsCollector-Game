494503180be87e8d58a6e91df71ed0d604894224
Jenny Chu <JennyChu@Jennys-MacBook-Pro.local>
2017-02-08 21:56:33 -0800

    edited possible values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 var game = {
-	possibleGameScore: [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72],
 	userScore: 0,
 	winsCount: 0,
 	losesCount: 0,
@@ -11,13 +10,19 @@ var game = {
 	possibleGemValue: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
 	currentAns: null,
 
+	// generate new gem value
 	genGemValue: function() {
 		return this.possibleGemValue[Math.floor(Math.random() * this.possibleGemValue.length)];
 	},
 
+	// generate random integer between 10 and 120
+	getRandomInt: function() {
+    		return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	},
+
 	newGame: function() {
 		// declare new answer user needs to get to
-		this.currentAns = this.possibleGameScore[Math.floor(Math.random() * this.possibleGameScore.length)];
+		this.currentAns = this.getRandomInt();
 		console.log("Current answer: " + this.currentAns);
 
 		// change ending score on screen
